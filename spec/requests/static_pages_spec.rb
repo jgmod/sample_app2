777b663059156f36b88dafd520a91e02255e0569
jgmod <jgmod@hotmail.com>
2013-02-26 23:11:02 -0500

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -31,13 +31,27 @@ describe "StaticPages" do
   end
 
 
-    describe "Contact page" do
-      before {visit contact_path}
-      it {should have_selector('h1', text: 'Contact')}
-      it {should have_selector('title',
-                      text: "Ruby on Rails Tutorial Sample App | Contact")}
+  describe "Contact page" do
+    before {visit contact_path}
+    it {should have_selector('h1', text: 'Contact')}
+    it {should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | Contact")}
+  end
+
+  describe "for signed in users" do
+    let(:user) {FactoryGirl.create(:user)}
+    before do
+      FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+      FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+      sign_in user
+      visit root_path
     end
 
-  
+    it "should render the user's feed" do
+      user.feed.each do |item|
+        page.should have_selector("li##{item.id}", text: item.content)
+      end
+    end
+  end
 
 end
