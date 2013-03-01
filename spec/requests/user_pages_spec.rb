fd103fae6c6a95a7a59271f1fe9aff4aa20d3196
John Goh <jgmod@hotmail.com>
2013-03-01 17:45:52 -0500

    stopped at 11.2.5

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -136,4 +136,80 @@ describe "UserPages" do
 			it {should have_content('error')}
 		end
 	end
+
+
+	describe "following/followed" do
+		let(:user){FactoryGirl.create(:user)}
+		let(:other_user){FactoryGirl.create(:user)}
+		before {user.follow!(other_user)}
+
+		describe "followed users" do
+			before {sign_in user}
+			before {visit following_user_path(user)}
+		
+
+			it{should have_selector('title', text: full_title('Following'))}
+			it{should have_selector('h3', text: 'Following')}
+			it{should have_link(other_user.name, href: user_path(other_user))}
+		end
+
+		describe "following users" do
+			before {sign_in other_user}
+			before {visit followers_user_path(other_user)}
+
+			it{should have_selector('title', text: full_title('Followers'))}
+			it{should have_selector('h3', text: 'Followers')}
+			it{should have_link(user.name, href:user_path(user))}
+		end
+
+	end
+
+	describe "follow/unfollow buttons" do
+		let(:other_user){FactoryGirl.create(:user)}
+		let(:user){FactoryGirl.create(:user)}
+		before {sign_in user}
+
+		describe "following a user" do
+			before {visit user_path(other_user)}
+			it "should increment followed user count" do
+				expect do
+					click_button "Follow"
+				end.to change(user.followed_users, :count).by(1)
+			end
+
+			it "should increment the other user's follower count" do
+				expect do
+					click_button "Follow"
+				end.to change(other_user.followers, :count).by(1)
+			end
+
+			describe "toggling the button" do
+				before {click_button "Follow"}
+				it {should have_selector('input', value: 'Unfollow')}
+			end
+		end
+
+		describe "unfollowing a user" do
+			before {user.follow!(other_user)}
+			before {visit user_path(other_user)}
+		
+			it "should decrement the followed user count" do
+				expect do
+					click_button "Unfollow"
+				end.to change(user.followed_users, :count).by(-1)
+			end
+
+			it "should decrement the other user's follower count" do
+				expect do
+					click_button "Unfollow"
+				end.to change(other_user.followers, :count).by(-1)
+			end
+
+			describe "toggling the button" do
+				before {click_button "Unfollow"}
+				it {should have_selector('input', value: 'Follow')}
+			end
+		end
+	end
+
 end
